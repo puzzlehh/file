e88f98fb6ac46f8d27d399dd9fa48d9a1ee15d10
zbj321 <13893834+zbj321@user.noreply.gitee.com>
2024-08-27 21:04:14 +0800

    feat: 解决跨域问题

---
+++ src/main/java/com/topview/file/common/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.topview.file.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description:
+ * @Author: zbj
+ * @Date: 2024/8/27
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 所有接口
+                .allowCredentials(true) // 是否发送 Cookie
+                .allowedOriginPatterns("*") // 支持域
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 支持方法
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
+
