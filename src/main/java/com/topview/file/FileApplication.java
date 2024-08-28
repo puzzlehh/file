296e1a811a0fc73e7f09c159d7fccc0c62fb9c0c
zbj321 <13893834+zbj321@user.noreply.gitee.com>
2024-08-28 10:30:45 +0800

    feat: 加注解mapperScan

--- src/main/java/com/topview/file/FileApplication.java
+++ src/main/java/com/topview/file/FileApplication.java
@@ -1,12 +1,13 @@
 package com.topview.file;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan(value = "com.topview.file.mapper")
 public class FileApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class, args);
     }
