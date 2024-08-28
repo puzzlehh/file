3be65adf0c7b22c1bda0904246ab87743320a6f0
zbj321 <13893834+zbj321@user.noreply.gitee.com>
2024-08-28 10:31:17 +0800

    feat: 暴漏文件dto

---
+++ src/main/java/com/topview/file/entity/dto/ExportFileDto.java
@@ -0,0 +1,17 @@
+package com.topview.file.entity.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: zbj
+ * @Date: 2024/8/27
+ */
+
+
+@Data
+public class ExportFileDto {
+    List<Integer> ids;
+}
