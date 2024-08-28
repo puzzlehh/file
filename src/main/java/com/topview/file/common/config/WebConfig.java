d64f61d1d25735fcc7c10fbf618b73d4cd19bbb0
zbj321 <13893834+zbj321@user.noreply.gitee.com>
2024-08-28 10:31:40 +0800

    feat: 支持所有跨域Methods

--- src/main/java/com/topview/file/common/config/WebConfig.java
+++ src/main/java/com/topview/file/common/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 所有接口
                 .allowCredentials(true) // 是否发送 Cookie
                 .allowedOriginPatterns("*") // 支持域
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 支持方法
+                .allowedMethods("*") // 支持方法
                 .allowedHeaders("*")
                 .exposedHeaders("*");
     }
